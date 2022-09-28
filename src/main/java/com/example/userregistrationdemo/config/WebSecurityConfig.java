1d224e8a8b48ce3ea531bc91216c94830b5e9d43
Salex <123@13.com>
2022-09-28 15:15:03 +0300

    Added spring security. Added WebSecurityConfig.

---
+++ src/main/java/com/example/userregistrationdemo/config/WebSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.example.userregistrationdemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests((requests) -> requests
+                        .antMatchers("/registration").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout((logout) -> logout.permitAll());
+        return httpSecurity.build();
+    }
+}
